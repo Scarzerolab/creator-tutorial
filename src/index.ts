45b1aea163baf4dcffde4a5363d2330de58cce72
trahwidhi <trahwidhi.oxtan17@gmail.com>
2026-01-08 20:40:49 +0800

    Autorization added

--- src/index.ts
+++ src/index.ts
@@ -45,7 +45,7 @@ app.use(limiter);
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, X-API-Key');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, X-API-Key, Authorization');
   next();
 });
 
